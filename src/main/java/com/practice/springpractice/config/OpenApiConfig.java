d085b5f9a6771c5efa038a4f2be09f421dd99b05
Dinu Rusu <dinu@codingshadows.com>
2023-08-23 10:14:40 +0300

    openapi config

---
+++ src/main/java/com/practice/springpractice/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.practice.springpractice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Spring Practice API")
+                .description("This is a Spring Boot practice project."));
+    }
+}
