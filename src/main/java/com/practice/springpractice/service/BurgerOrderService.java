6a2466f836a89fa15fce982c12994d75fa48f52f
Tudor <tudorciot@gmail.com>
2023-08-24 16:52:20 +0300

    Logger implemented!

--- src/main/java/com/practice/springpractice/service/BurgerOrderService.java
+++ src/main/java/com/practice/springpractice/service/BurgerOrderService.java
@@ -4,11 +4,12 @@ import com.practice.springpractice.model.Burger;
 import com.practice.springpractice.model.BurgerOrder;
 import com.practice.springpractice.repository.BurgerOrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 
@@ -22,11 +23,13 @@ public class BurgerOrderService {
 
 
     public List<BurgerOrder> getAllOrders() {
+        log.info("Get all orders.");
         return burgerOrderRepository.findAll();
     }
 
     public BurgerOrder addBurgerOrder(Long id, Integer quantity) {
 
+        log.info("Creating order for burger with id " + id);
         Burger burger = burgerService.getBurger(id);
 
         return burgerOrderRepository.save(  new BurgerOrder( null , quantity, id , (double) burger.getPrice() * quantity ));
--- src/main/java/com/practice/springpractice/service/BurgerService.java
+++ src/main/java/com/practice/springpractice/service/BurgerService.java
@@ -4,10 +4,12 @@ package com.practice.springpractice.service;
 import com.practice.springpractice.model.Burger;
 import com.practice.springpractice.repository.BurgerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BurgerService {
@@ -17,24 +19,29 @@ public class BurgerService {
 
     public Burger addBurger( Burger burger)
     {
+        log.info("Creating burger");
         return burgerRepository.save(burger);
     }
 
 
     public Burger updateBurger(Burger burger) {
+        log.info("Updating burger");
         return burgerRepository.save(burger);
     }
 
     public void deleteBurger(Long id) {
+        log.info("Deleting burger");
         burgerRepository.deleteById(id);
     }
 
     public Burger getBurger(Long id) {
+        log.info("Getting burger with id " + id);
 
         return burgerRepository.findById( id ).get();
     }
 
     public List<Burger> getAllBurgers() {
+        log.info("Listing all burgers");
         return burgerRepository.findAll();
     }
 }
