7409c0387250525490795f89762765062282d87a
Vlad Stefan Iacob <vladiacob14@gmail.com>
2023-08-25 15:54:43 +0300

    cookies for user

--- src/main/java/com/practice/springpractice/controller/UserController.java
+++ src/main/java/com/practice/springpractice/controller/UserController.java
@@ -1,12 +1,16 @@
+
+
 package com.practice.springpractice.controller;
 
 import com.practice.springpractice.model.User;
 import com.practice.springpractice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -15,8 +19,15 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("register")
-    public User registerUser(@RequestBody User user) {
-        return userService.saveUser(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        ResponseCookie cookie = ResponseCookie.from("username", user.getUsername()).secure(false).maxAge(Duration.ofDays(7)).build();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(userService.saveUser(user));
+    }
+
+    @PostMapping("login-no-username")
+    public boolean logNoUsername(@RequestBody User user, @CookieValue(name = "username", defaultValue = "default") String username) {
+        user.setUsername(username);
+        return userService.logIn(user);
     }
 
     @PostMapping("login")
@@ -24,3 +35,4 @@ public class UserController {
         return userService.logIn(user);
     }
 }
+
